c101e270e65164e7381bb0cf448ba699ed471697
milankyncl <milan.kyncl@icloud.com>
2018-03-16 17:47:43 +0100

    ares api class

--- src/AresAPI.php
+++ src/AresAPI.php
@@ -10,7 +10,44 @@ namespace MilanKyncl;
 
 class AresAPI {
 
+	const BACKEND_URL = 'https://wwwinfo.ares.cz';
+
 	public function findByName() {
+
+	}
+
+	/**
+	 * Create API Request
+	 * ------------------
+	 *
+	 * Create API request, parse XML feed and return array.
+	 *
+	 * @return array
+	 *
+	 * @throws \Exception
+	 */
+
+	private function _createRequest($parameters) {
+
+		$curl = curl_init(self::BACKEND_URL);
+
+		curl_setopt_array($curl, [
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_SSL_VERIFYPEER => 0
+		]);
+
+		$content = curl_exec($curl);
+
+		if (!$content)
+			throw new \Exception(curl_error($curl), curl_errno($curl));
+
+		curl_close($curl);
+
+		/**
+		 * Parse XML now
+		 */
+
+		return $content;
 
 	}
 
